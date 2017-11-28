502b5b954bde0fb76d2d8221bf5c8a6dd2947d8a
Daniel Knight <goodknightDK@gmail.com>
2017-11-27 22:11:42 -0800

    prod media files fix

--- ross_web/urls.py
+++ ross_web/urls.py
@@ -26,7 +26,9 @@ urlpatterns = [
                                namespace='rest_framework')),
     url(r'^\.well-known/', include('letsencrypt.urls')),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
